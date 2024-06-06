1d68173b9d72c3d9dc28f11c314e8268ca456984
Carlos Rafael Gimenes das Neves <carlosrafael.prog@gmail.com>
2024-06-06 08:52:25 -0300

    Ajustes de backend, imagens e markers

--- routes/index.ts
+++ routes/index.ts
@@ -62,6 +62,35 @@ class IndexRoute {
 		res.render("index/novo", opcoes);
 	}
 
+	public async editar(req: app.Request, res: app.Response) {
+		let idlocal = parseInt(req.query["idlocal"] as string);
+
+		let local: any = null;
+
+		let tipos: any[];
+
+		await app.sql.connect(async sql => {
+			tipos = await sql.query("select idtipo, nm_tipo from tipo order by nm_tipo");
+			let lista: any[] = await sql.query("select idlocal, idtipo, nm_local, end_local, cep_local, num_local, cidade_local, uf_local, lat_local, lng_local from local where idlocal = ?", [idlocal]);
+			if (lista.length) {
+				local = lista[0];
+			}
+		});
+
+		let opcoes = {
+			titulo: "Editar Local",
+			tipos: tipos,
+			local: local
+		};
+
+		if (!local) {
+			res.render("index/nao-encontrado", opcoes);
+			return;
+		}
+
+		res.render("index/editar", opcoes);
+	}
+
 	@app.http.post()
 	@app.route.formData()
 	public async adicionar(req: app.Request, res: app.Response) {
@@ -119,7 +148,67 @@ class IndexRoute {
 		res.json(true);
 	}
 
-	
+	@app.http.post()
+	@app.route.formData()
+	public async salvar(req: app.Request, res: app.Response) {
+		let local = req.body;
+
+		if (!local.idlocal) {
+			res.status(400).json("Id inválido!");
+			return;
+		}
+
+		if (!local.nm_local) {
+			res.status(400).json("Nome inválido!");
+			return;
+		}
+
+		if (!local.end_local) {
+			res.status(400).json("Endereço inválido!");
+			return;
+		}
+
+		if (!local.cep_local) {
+			res.status(400).json("cep inválido!");
+			return;
+		}
+
+		if (!local.num_local) {
+			res.status(400).json("Numero inválido!");
+			return;
+		}
+
+		if (!local.cidade_local) {
+			res.status(400).json("CIdade inválida!");
+			return;
+		}
+
+		if (!local.uf_local) {
+			res.status(400).json("Estado inválido!");
+			return;
+		}
+
+		if (!local.idtipo) {
+			res.status(400).json("Tipo inválido!");
+			return;
+		}
+
+		if (!local.lat_local) {
+			res.status(400).json("Latitude inválida!");
+			return;
+		}
+
+		if (!local.lng_local) {
+			res.status(400).json("Longitude inválida!");
+			return;
+		}
+
+		await app.sql.connect(async sql => {
+			await sql.query("update local set idtipo = ?, nm_local = ?, end_local = ?, cep_local = ?, num_local = ?, cidade_local = ?, uf_local = ?, lat_local = ?, lng_local = ? where idlocal = ?", [ local.idtipo, local.nm_local, local.end_local, local.cep_local, local.num_local, local.cidade_local, local.uf_local, local.lat_local, local.lng_local, local.idlocal ]);
+		});
+
+		res.json(true);
+	}
 
 	public async locais(req: app.Request, res: app.Response) {
 		let tipos: any[];
